ea6bcf4a9ef2baf3a783f94db2081870c4b11ff8
rafaelacerlat <rafaela.cerlat@gmail.com>
2022-09-28 21:39:48 +0300

    error fixes

--- src/Lab1/Main.java
+++ src/Lab1/Main.java
@@ -14,29 +14,34 @@ public class Main {
         int key = 13;
         String encrypted;
 
+        System.out.println("The plaintext: " + message);
+
         Cipher cipher1 = new CaesarCipher(13);
         encrypted = cipher1.encrypt(message);
 
-        System.out.println(encrypted);
-        System.out.println(cipher1.decrypt(encrypted) + "\n");
+        System.out.println("Caesar Cipher:");
+        System.out.println("Encrypted: " + encrypted);
+        System.out.println("Decrypted: " + cipher1.decrypt(encrypted) + "\n");
 
         Cipher cipher2 = new VigenereCipher(message, keyword);
         encrypted = cipher2.encrypt(message);
 
-        System.out.println(encrypted);
-        System.out.println(cipher2.decrypt(encrypted));
+        System.out.println("Vigenere Cipher:");
+        System.out.println("Encrypted: " + encrypted);
+        System.out.println("Decrypted: " + cipher2.decrypt(encrypted));
 
         Cipher cipher3 = new CaesarCipherWithPermutation(key, keyword);
         encrypted = cipher3.encrypt(message);
 
-        System.out.println("\n" + encrypted);
-        System.out.println(cipher3.decrypt(encrypted));
+        System.out.println("\nCaesar Cipher with permutation:");
+        System.out.println("Encrypted: " + encrypted);
+        System.out.println("Decrypted: " + cipher3.decrypt(encrypted));
 
         Cipher cipher4 = new PlayfairCipher(keyword);
         encrypted = cipher4.encrypt(message);
 
-        System.out.println("\n" + encrypted);
-        System.out.println(cipher4.decrypt(encrypted));
+        System.out.println("Encrypted: " + encrypted);
+        System.out.println("Decrypted: " + cipher4.decrypt(encrypted));
 
 
 
