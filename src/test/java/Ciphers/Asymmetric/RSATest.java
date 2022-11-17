d0e28bc2ebd98c798c0bea14f59e90f0e4d7a62c
rafaelacerlat <rafaela.cerlat@gmail.com>
2022-11-17 18:58:04 +0200

    changes in the project structure

--- src/test/java/Ciphers/Asymmetric/RSATest.java
+++ src/test/java/Ciphers/Asymmetric/RSATest.java
@@ -1,11 +1,22 @@
 package Ciphers.Asymmetric;
 
+import Ciphers.Cipher;
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
 
 class RSATest {
 
     @Test
     void testRSA() {
+        String message = "This is a secret message";
 
+        Cipher asymmetricCipher = new RSA(345);
+        String encrypted = asymmetricCipher.encrypt(message);
+        String decrypted = asymmetricCipher.decrypt(encrypted);
+
+        System.out.println("RC4 Encrypted: " + encrypted);
+        System.out.println("Decrypted: " + decrypted);
+
+        assertEquals(message, decrypted);
     }
 }
